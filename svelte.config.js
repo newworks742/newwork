e23e171fea26819dd3e05af9bcf873b3e0424e2d
Bhavana R <bhavanabanu2028@gmail.com>
2025-08-26 15:28:37 +0530

    svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,42 @@
-// import adapter from '@sveltejs/adapter-auto';
+// // import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { preprocessMeltUI, sequence } from '@melt-ui/pp';
+// import adapter from '@sveltejs/adapter-node';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			out: 'out',
+// 			precompress: true,
+// 			envPrefix: 'Digitoad_',
+// 			assets: 'static'
+// 		}),
+// 		csrf: {
+// 			checkOrigin: false
+// 		}
+// 	},
+// 	preprocess: [vitePreprocess(), sequence([preprocessMeltUI()])]
+// };
+// export default config;
+
+
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-netlify'; // 👈 use Netlify adapter
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			out: 'out',
-			precompress: true,
-			envPrefix: 'Digitoad_',
-			assets: 'static'
-		}),
-		csrf: {
-			checkOrigin: false
-		}
-	},
-	preprocess: [vitePreprocess(), sequence([preprocessMeltUI()])]
+  kit: {
+    adapter: adapter({
+      // you usually don't need to set options, defaults are good
+    }),
+    csrf: {
+      checkOrigin: false
+    }
+  },
+  preprocess: [vitePreprocess(), sequence([preprocessMeltUI()])]
 };
+
 export default config;
